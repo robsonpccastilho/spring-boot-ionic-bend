ea074ff1118704e3452203ba85d4fd925e538dd4
Robson Castilho <estudo@rcastilho.com.br>
2020-03-31 19:23:03 -0300

    Listando todas as categorias com DTO

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.nelioalves.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.dto.CategoriaDTO;
 import com.nelioalves.cursomc.services.CategoriaService;
 
 @RestController
@@ -27,19 +30,8 @@ public class CategoriaResource {
 		Categoria obj = service.find(id);
 		
 		return ResponseEntity.ok().body(obj);
-		
-		/* -------------------------------------teste anterior
-		//Categoria cat1 = new Categoria(1, "Informática");
-		//Categoria cat2 = new Categoria(2, "Escrtitório");
-
-		//List<Categoria> lista = new ArrayList<Categoria>();
-		//lista.add(cat1);
-		//lista.add(cat2);
-			
-	    //return lista;
-		------------------------------------------------------ */
-	    /*return "REST está funcionando...";*/
 	}
+	
 	@RequestMapping(method=RequestMethod.POST)
 	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
 		obj = service.insert(obj);
@@ -61,5 +53,12 @@ public class CategoriaResource {
 	{
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 }
