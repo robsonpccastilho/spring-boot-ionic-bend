8e3e9be5faa6f71333b2b0aa1b4f0d3f0a462fe0
Robson Castilho <estudo@rcastilho.com.br>
2020-04-02 19:02:19 -0300

    Paginacao com paremetros opcionais na requisicao

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -68,8 +68,13 @@ public class CursomcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-		Categoria cat1 = new Categoria(null, "Inform·tica");
-		Categoria cat2 = new Categoria(null, "EscritÛrio");
+		Categoria cat1 = new Categoria(null, "Inform√°tica");
+		Categoria cat2 = new Categoria(null, "Escrit√≥rio");
+		Categoria cat3 = new Categoria(null, "Cama mesa e banho");
+		Categoria cat4 = new Categoria(null, "Eletr√¥nicos");
+		Categoria cat5 = new Categoria(null, "Jardinagem");
+		Categoria cat6 = new Categoria(null, "Decora√ß√£o");
+		Categoria cat7 = new Categoria(null, "Perfumaria");
 		
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
@@ -82,14 +87,14 @@ public class CursomcApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 		Estado est1 = new Estado(null, "Minas Gerais");
-		Estado est2 = new Estado(null, "S„o Paulo");
+		Estado est2 = new Estado(null, "SÔøΩo Paulo");
 		
 		Cidade c1 = new Cidade(null, "Uberlandia", est1);
-		Cidade c2 = new Cidade(null, "S„o Paulo", est2);
+		Cidade c2 = new Cidade(null, "SÔøΩo Paulo", est2);
 		Cidade c3 = new Cidade(null, "Campinas", est2);
 		
 		est1.getCidades().addAll(Arrays.asList(c1));
