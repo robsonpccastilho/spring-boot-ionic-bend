7df3bd0e505354ae233b9886adda48d18217f7d4
Robson Castilho <estudo@rcastilho.com.br>
2020-04-11 11:48:03 -0300

    Validacao customizada: email nao reportado na insercao de Cliente

--- src/main/java/com/nelioalves/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/nelioalves/cursomc/repositories/ClienteRepository.java
@@ -2,10 +2,15 @@ package com.nelioalves.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.nelioalves.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
+	//Colocando o nome do campo depois de findBy automaticamente faz e procura pelo campo indicado passando o campo como parametro
+	// Ex.: findBy + email -> findByEmail
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 }
