e130aaa609d36ecefc9cecd5d1d597f490b0c8a7
Robson Castilho <estudo@rcastilho.com.br>
2020-04-11 17:07:38 -0300

    INSERINDO PEDIDO

--- src/main/java/com/nelioalves/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/nelioalves/cursomc/domain/PagamentoComCartao.java
@@ -2,8 +2,10 @@ package com.nelioalves.cursomc.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.nelioalves.cursomc.domain.enums.EstadoPagamento;
 @Entity
+@JsonTypeName("pagamentoComCartao") 
 public class PagamentoComCartao extends Pagamento {
 	private static final long serialVersionUID = 1L;
 	private Integer numeroDeParcelas;
