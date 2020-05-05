df54d82f4d99cb6f381df93d4621a3970a29a742
Robson Castilho <estudo@rcastilho.com.br>
2020-05-05 18:25:53 -0300

    Esqueci a senha

--- src/main/java/com/nelioalves/cursomc/services/AbstractEmailService.java
+++ src/main/java/com/nelioalves/cursomc/services/AbstractEmailService.java
@@ -2,11 +2,10 @@ package com.nelioalves.cursomc.services;
 
 import java.util.Date;
 
-import java.util.Date;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 
+import com.nelioalves.cursomc.domain.Cliente;
 import com.nelioalves.cursomc.domain.Pedido;
 
 public abstract class AbstractEmailService implements EmailService {
@@ -29,4 +28,22 @@ public abstract class AbstractEmailService implements EmailService {
 		sm.setText(obj.toString());
 		return sm;
 	}
+	
+	@Override
+	public void  sendNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = prepareNewPasswordEmail(cliente,newPass);
+		sendEmail(sm);
+	}
+	
+	protected SimpleMailMessage prepareNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = new SimpleMailMessage();
+		sm.setTo(cliente.getEmail());
+		sm.setFrom(sender);
+		sm.setSubject("Solicitação de Nova Senha!");
+		sm.setSentDate(new Date(System.currentTimeMillis()));
+		sm.setText("Nova senha : " + newPass);
+		return sm;
+	}
+	
+	
 }
